253da61ec38857dd2c4a2b8b2c219263cafe6031
Phodal Huang <h@phodal.com>
2020-03-04 16:28:59 +0800

    fix: fix build

--- src/app/shared/modules/mind-map/mind-map/mind-map.component.spec.ts
+++ src/app/shared/modules/mind-map/mind-map/mind-map.component.spec.ts
@@ -1,11 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 import { MindMapComponent } from './mind-map.component';
 import { SharedModule } from '../../../shared.module';
 import { FeaturesModule } from '../../../../features/features.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('MindmapComponent', () => {
   let component: MindMapComponent;
