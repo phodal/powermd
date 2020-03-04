d815fc680c0f6612ff4917cf63f0ee5fc2b77881
Phodal Huang <h@phodal.com>
2020-03-04 16:56:27 +0800

    refactor: draw logic

--- src/app/shared/markdown/markdown-radar-chart/markdown-radar-chart.component.ts
+++ src/app/shared/markdown/markdown-radar-chart/markdown-radar-chart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MarkdownTaskModel } from '../model/markdown.model';
 import { MarkdownTaskItemService } from '../markdown-task-item/markdown-task-item.service';
 
@@ -11,6 +11,7 @@ import d3 from 'd3';
   styleUrls: ['./markdown-radar-chart.component.scss']
 })
 export class MarkdownRadarChartComponent implements OnInit, AfterViewInit {
+  @ViewChild('baseElement', {}) baseElement: ElementRef;
   tasks: MarkdownTaskModel[];
 
   constructor(private markdownTaskItemService: MarkdownTaskItemService) {
@@ -26,81 +27,34 @@ export class MarkdownRadarChartComponent implements OnInit, AfterViewInit {
 
   /* tslint:disable */
   render() {
+    const element = this.baseElement.nativeElement;
+
     const w = 500;
     const h = 500;
 
     let colorscale = d3.scale.category10();
 
-// Legend titles
     let LegendOptions = ['Smartphone', 'Tablet'];
-
-// Data
-    let d = [
+    let data = [
       [
-        {axis: 'Email', value: 0.59},
-        {axis: 'Social Networks', value: 0.56},
-        {axis: 'Internet Banking', value: 0.42},
-        {axis: 'News Sportsites', value: 0.34},
-        {axis: 'Search Engine', value: 0.48},
-        {axis: 'View Shopping sites', value: 0.14},
-        {axis: 'Paying Online', value: 0.11},
-        {axis: 'Buy Online', value: 0.05},
-        {axis: 'Stream Music', value: 0.07},
-        {axis: 'Online Gaming', value: 0.12},
-        {axis: 'Navigation', value: 0.27},
-        {axis: 'App connected to TV program', value: 0.03},
-        {axis: 'Offline Gaming', value: 0.12},
-        {axis: 'Photo Video', value: 0.4},
-        {axis: 'Reading', value: 0.03},
-        {axis: 'Listen Music', value: 0.22},
-        {axis: 'Watch TV', value: 0.03},
-        {axis: 'TV Movies Streaming', value: 0.03},
-        {axis: 'Listen Radio', value: 0.07},
-        {axis: 'Sending Money', value: 0.18},
-        {axis: 'Other', value: 0.07},
-        {axis: 'Use less Once week', value: 0.08}
-      ], [
-        {axis: 'Email', value: 0.48},
-        {axis: 'Social Networks', value: 0.41},
-        {axis: 'Internet Banking', value: 0.27},
-        {axis: 'News Sportsites', value: 0.28},
-        {axis: 'Search Engine', value: 0.46},
-        {axis: 'View Shopping sites', value: 0.29},
-        {axis: 'Paying Online', value: 0.11},
-        {axis: 'Buy Online', value: 0.14},
-        {axis: 'Stream Music', value: 0.05},
-        {axis: 'Online Gaming', value: 0.19},
-        {axis: 'Navigation', value: 0.14},
-        {axis: 'App connected to TV program', value: 0.06},
-        {axis: 'Offline Gaming', value: 0.24},
-        {axis: 'Photo Video', value: 0.17},
-        {axis: 'Reading', value: 0.15},
-        {axis: 'Listen Music', value: 0.12},
-        {axis: 'Watch TV', value: 0.1},
-        {axis: 'TV Movies Streaming', value: 0.14},
-        {axis: 'Listen Radio', value: 0.06},
-        {axis: 'Sending Money', value: 0.16},
-        {axis: 'Other', value: 0.07},
-        {axis: 'Use less Once week', value: 0.17}
-      ]
+        {axis: 'Email', value: 4},
+        {axis: 'Social Networks', value: 2},
+        {axis: 'Internet Banking', value: 3},
+        {axis: 'News Sportsites', value: 2},
+        {axis: 'Search Engine', value: 5}
+      ], []
     ];
 
-// Options for the Radar chart, other than default
+    // Options for the Radar chart, other than default
     let mycfg = {
-      w,
-      h,
-      maxValue: 0.6,
-      levels: 6,
+      width: w,
+      height: h,
+      maxValue: 5,
+      levels: 5,
       ExtraWidthX: 300
     };
 
-// Call function to draw the Radar chart
-// Will expect that data is in %'s
-    RadarChart.draw('#chart', d, mycfg);
-
-////////////////////////////////////////////
-/////////// Initiate legend ////////////////
-////////////////////////////////////////////
+    RadarChart.draw('#chart', data, mycfg);
 
     let svg = d3.select('#body')
       .selectAll('svg')
@@ -108,24 +62,13 @@ export class MarkdownRadarChartComponent implements OnInit, AfterViewInit {
       .attr('width', w + 300)
       .attr('height', h);
 
-// Create the title for the legend
-    let text = svg.append('text')
-      .attr('class', 'title')
-      .attr('transform', 'translate(90,0)')
-      .attr('x', w - 70)
-      .attr('y', 10)
-      .attr('font-size', '12px')
-      .attr('fill', '#404040')
-      .text('What % of owners use a specific service in a week');
-
-// Initiate Legend
     let legend = svg.append('g')
       .attr('class', 'legend')
       .attr('height', 100)
       .attr('width', 200)
       .attr('transform', 'translate(90,20)')
     ;
-    // Create colour squares
+
     legend.selectAll('rect')
       .data(LegendOptions)
       .enter()
@@ -138,7 +81,7 @@ export class MarkdownRadarChartComponent implements OnInit, AfterViewInit {
         return colorscale(i.toString());
       })
     ;
-    // Create text next to squares
+
     legend.selectAll('text')
       .data(LegendOptions)
       .enter()
